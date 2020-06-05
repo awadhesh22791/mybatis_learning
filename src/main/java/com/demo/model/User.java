13f07a823b525d4f5030afa1eceef3cfcd1fd3c2
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-06-05 21:38:44 +0530

    Added unit tests for remaining methods of user repository.

--- src/main/java/com/demo/model/User.java
+++ src/main/java/com/demo/model/User.java
@@ -1,11 +1,15 @@
 package com.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 @Setter
 @ToString
--- src/test/java/com/demo/repository/UserRepositoryTests.java
+++ src/test/java/com/demo/repository/UserRepositoryTests.java
@@ -1,6 +1,9 @@
 package com.demo.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
@@ -18,6 +21,34 @@ public class UserRepositoryTests {
 	@Test
 	public void findAllTest() {
 		List<User> allUser = userRepository.findAll();
-		assertEquals(allUser.size(), 1);
+		assertTrue(allUser.size()>0);
+	}
+	
+	@Test
+	public void findByIdTest() {
+		User firstUser=userRepository.findById(1);
+		assertNotNull(firstUser);
+		assertEquals(1, firstUser.getId());
+		User noUser=userRepository.findById(22);
+		assertNull(noUser);
+	}
+	
+	@Test
+	public void insertTest() {
+		User firstUser=User.builder().firstName("Shannon").lastName("Holland").email("shannon.holland@email.com").build();
+		int newUserId=userRepository.insert(firstUser);
+		User foundFirstUser=userRepository.findById(newUserId);
+		assertNotNull(foundFirstUser);
+	}
+	
+	@Test
+	public void updateTest() {
+		User newUser=User.builder().firstName("Martha").lastName("Day").email("martha.day@email.com").build();
+		int newUserId=userRepository.insert(newUser);
+		User existingUser=userRepository.findById(newUserId);
+		existingUser.setFirstName("Layla");
+		userRepository.update(existingUser);
+		existingUser=userRepository.findById(newUserId);
+		assertEquals("Layla", existingUser.getFirstName());
 	}
 }
