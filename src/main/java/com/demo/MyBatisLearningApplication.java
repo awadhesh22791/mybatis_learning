ae52d467a59c5e848bdbbd4753dfc91f9986e487
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-06-05 19:49:27 +0530

    Cleanup command line runner.

--- src/main/java/com/demo/MyBatisLearningApplication.java
+++ src/main/java/com/demo/MyBatisLearningApplication.java
@@ -2,41 +2,13 @@ package com.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.demo.model.User;
-import com.demo.repository.UserRepository;
-
 @SpringBootApplication
-public class MyBatisLearningApplication implements CommandLineRunner {
+public class MyBatisLearningApplication{
 	Logger logger=LoggerFactory.getLogger(this.getClass());
 	public static void main(String[] args) {
 		SpringApplication.run(MyBatisLearningApplication.class, args);
 	}
-
-	@Autowired
-	private UserRepository userRepository;
-
-	@Override
-	public void run(String... args) throws Exception {
-		User newUser=new User();
-		newUser.setEmail("jennifer.james@email.com");
-		newUser.setFirstName("Jennifer");
-		newUser.setLastName("James");
-		int newUserId=userRepository.insert(newUser);
-		logger.info("Inserting -> {}", newUserId);
-		logger.info("Inserted user -> {}",userRepository.findAll().toString());
-		User existingUser=userRepository.findById(newUserId);
-		logger.info("Found user -> {}",existingUser.toString());
-		existingUser.setFirstName("Anna");
-		existingUser.setLastName("Howard");
-		existingUser.setEmail("anna.howard@email.com");
-		logger.info("Updating user -> {}",userRepository.update(existingUser));
-		logger.info("Update user -> {}",userRepository.findById(existingUser.getId()).toString());
-		logger.info("Deleting user -> {}",userRepository.delete(newUserId));
-	}
-
 }
